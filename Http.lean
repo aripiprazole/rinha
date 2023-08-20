da707fff8295f46926193317c37a9f3fc74ab562
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-08-20 13:07:18 +0000

    feat: add basic data types

---
+++ Http.lean
@@ -0,0 +1,3 @@
+namespace Http
+
+end Http
--- Rinha.lean
+++ Rinha.lean
@@ -1,1 +1,51 @@
 def hello := "world"
+
+namespace Rinha
+/--
+The *basic* type of a person. It contains it's name and other info
+about the person.
+-/
+structure Person where
+  username : String
+  name : Name
+  age : Nat
+  birthdate : String
+  stack : Option (List Stack) := none
+
+instance : ToString Person where
+  toString p :=
+    "Person {username: " ++ p.username ++
+      ", name: " ++ p.name ++
+      ", age: " ++ toString p.age ++
+      ", birthdate: " ++ (toString p.birthdate)
+
+/--
+The username of a person. It contains the username of the person and it can
+be only 32 characters long.
+
+It have to be unique.
+-/
+structure Username where
+  data : String
+  not_empty : (data.length > 0) = True
+  less_than : (data.length <= 32) = True
+
+/--
+The name of a person. It contains the name of the person and it can
+be only 100 characters long.
+-/
+structure Name where
+  data : String
+  not_empty : (data.length > 0) = True
+  less_than : (data.length <= 100) = True
+
+/--
+The stack of a person. It contains the name of the stack and it can
+be only 32 characters long.
+-/
+structure Stack where
+  data : String
+  not_empty : (data.length > 0) = True
+  less_than : (data.length <= 32) = True
+
+end Rinha
