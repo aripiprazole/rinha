c76238b71e63b3fbd8a810fc6c1501099f6202b9
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-08-21 21:34:13 +0000

    feat: add basic app

--- Main.lean
+++ Main.lean
@@ -1,4 +1,12 @@
 import Rinha
+import Ash
+
+open Ash.App
+
+def app : Ash.App Unit := do
+  get "/" $ fun conn => do
+    conn.ok "Hello, world"
 
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  app.run "localhost" "8000" do
+    IO.println "Server running on port 8000"
--- lakefile.lean
+++ lakefile.lean
@@ -16,4 +16,5 @@ lean_exe rinha {
 require lina from git "git@github.com:algebraic-sofia/lina.git"
 require soda from git "git@github.com:algebraic-sofia/soda.git"
 require melp from git "git@github.com:algebraic-sofia/melp.git"
+require ash from git "git@github.com:algebraic-sofia/ash.git"
 require pgsql from git "git@github.com:aripiprazole/pgsql.git"
